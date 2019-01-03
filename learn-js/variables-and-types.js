815887434113b9789dc0cdf2b0b580934f4890f8
John McIntosh II <jmci10@hotmail.com>
2019-01-02 20:33:35 -0500

    vairables and types

---
+++ learn-js/variables-and-types.js
@@ -0,0 +1,8 @@
+// Yay, for variables and types!
+let myNumber = 4;
+let myString = "Variables are great.";
+let myBoolean = false;
+
+console.log("myNumber is equal to " + myNumber);
+console.log("myString is equal to " + myString);
+console.log("myBoolean is equal to " + myBoolean);
