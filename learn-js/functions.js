398a1112d46d96fd32be6d7ccf20f0ccbba56f34
John McIntosh II <jmci10@hotmail.com>
2019-01-05 20:27:26 -0500

    functions

---
+++ learn-js/functions.js
@@ -0,0 +1,8 @@
+var multiplyFive = function(number) {
+	return number * 5;
+}
+
+console.log(multiplyFive(3));
+console.log(multiplyFive(4));
+console.log(multiplyFive(5));
+console.log(multiplyFive(6));
