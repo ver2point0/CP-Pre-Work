e8f152c80aa8c20db886f647d2081b2666127e2f
John McIntosh II <jmci10@hotmail.com>
2019-01-05 20:11:22 -0500

    conditions

---
+++ learn-js/conditions.js
@@ -0,0 +1,12 @@
+function checkNumber(num)
+{
+    if (num === 42) {
+        console.log("correct");
+    } else {
+        console.log("incorrect");
+    }
+}
+
+checkNumber(3);
+checkNumber(7);
+checkNumber(42);
