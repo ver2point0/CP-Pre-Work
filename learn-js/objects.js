5bb942ecbcd8232349a88f13fc64e0994ac620d3
John McIntosh II <jmci10@hotmail.com>
2019-01-05 20:25:09 -0500

    objects

---
+++ learn-js/objects.js
@@ -0,0 +1,11 @@
+var person = {
+    firstName : "Jack",
+    lastName : "Smith",
+    age	: 19,
+    employed : true
+};
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.employed);
