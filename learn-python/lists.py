19bcd9a7161207c872147892d3f5cb14b2c55ff5
John McIntosh II <jmci10@hotmail.com>
2019-01-11 08:06:54 -0500

    lists

---
+++ learn-python/lists.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3]
+strings = ["hello", "world"]
+names = ["John", "Eric", "Jessica"]
+
+second_name = names[1]
+
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
